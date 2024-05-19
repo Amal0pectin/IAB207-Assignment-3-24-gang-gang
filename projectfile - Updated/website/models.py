12b05a558be246fefbc026112d82730f69bd1542
ethanwilson0024 <n10534202@qut.edu.au>
2024-05-19 13:37:16 +1000

    Update models.py

--- projectfile - Updated/website/models.py
+++ projectfile - Updated/website/models.py
@@ -3,9 +3,51 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+    __tablename = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    comments = db.relationship('Comment', backref='user')
+
+    # string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
 
 class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(400))
+    eventdatetime = db.Column(db.DateTime)
+    # ... Create the Comments db.relationship
+	# relation to call destination.comments and comment.destination
+    comments = db.relationship('Comment', backref='destination')
+    
+    # string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
 
 class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(400))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    # add the foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
+
+    # string print method
+    def __repr__(self):
+        return f"Comment: {self.text}"
 
 class Order(db.Model):
+    _tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(400))
+    eventdatetime = db.Column(db.DateTime)
+    booked_at = db.Column(db.DateTime, default=datetime.now())
+    
