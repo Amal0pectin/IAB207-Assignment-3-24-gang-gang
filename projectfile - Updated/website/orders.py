0ebcd973a2e6a4e094a58a151be0fbc96aefd67f
Tung Pham <n11822228@qut.edu.au>
2024-05-30 13:28:39 +1000

    #

--- projectfile - Updated/website/orders.py
+++ projectfile - Updated/website/orders.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, abort
 from .models import Event, Comment, User, Order
-from .forms import EventForm, CommentForm
+from .forms import EventForm, CommentForm, UpdateForm
 from . import db
 import os
 from werkzeug.utils import secure_filename
@@ -8,12 +8,14 @@ from flask_login import login_required, current_user
 
 order_bp = Blueprint('Order', __name__, url_prefix="/Order")
 
+
+@order_bp.route('/<id>')
 @login_required
-@order_bp.route('/BookingHistory')
-def orders():
+def orders(id):
     # eventually we will query the database table 'destinations'for this id
     ## and get back all relevant information for that destination
-    orders = db.session.scalars(db.select(Order).join(Event).where(Order.user_id==current_user.id)).all()
-
-    print(orders)
-    return render_template(url_for('orders.html', id=id))
+    orders = db.session.scalars(db.select(Order).where(Order.id==id))
+    form = UpdateForm()
+    if not orders:
+        abort(404)
+    return render_template(url_for('Order.orders', id=id))
